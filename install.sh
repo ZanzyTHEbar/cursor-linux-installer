c504ca1479395c8a76c369983b6b63602f125a19
ZanzyTHEbar <zacariahheim@gmail.com>
2025-10-17 23:02:53 +0100

    chore: Update installation script URLs to point to personal repository for testing

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 # URL of the cursor.sh script in the GitHub repository
-CURSOR_SCRIPT_URL="https://raw.githubusercontent.com/ZanzyTHEbar/cursor-linux-installer/main/cursor.sh"
+CURSOR_SCRIPT_URL="https://raw.githubusercontent.com/ZanzyTHEbar/cursor-linux-installer/personal/cursor.sh"
 
 # Local bin directory
 LOCAL_BIN="$HOME/.local/bin"
